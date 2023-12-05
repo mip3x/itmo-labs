ab728839f110616f36bb5a27a57f601d2a47d3b4
mip3x <plombiiiri@yandex.ru>
2023-12-05 17:38:38 +0100

    main.py modified

--- first-course/informatics/lab4/main_task/main.py
+++ first-course/informatics/lab4/main_task/main.py
@@ -7,15 +7,6 @@ if __name__ == "__main__":
     output_file_name = DATA_DIRECTORY + "output.xml"
 
     input_file_data = open(input_file_name, "r").read()
-    # for i in input_file_data[:100]:
-    #     if i == ' ':
-    #         print('SPACE')
-    #     elif i == '\n':
-    #         print('NEWLINE')
-    #     elif i == '\t':
-    #         print('TAB')
-    #     else:
-    #         print(i)
 
     with open(output_file_name, 'w') as output_file:
         parse(input_file_data, output_file)
