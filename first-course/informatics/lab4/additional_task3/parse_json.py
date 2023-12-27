138b4e8e618e3515f90da434bc28bd8b807ea45d
mip3x <plombiiiri@yandex.ru>
2023-12-27 20:12:00 +0100

    🍄 lab4 informatics fixes

--- first-course/informatics/lab4/additional_task3/parse_json.py
+++ first-course/informatics/lab4/additional_task3/parse_json.py
@@ -1,4 +1,4 @@
-from pyparsing import Word, Suppress, nums, Group, delimitedList, Dict, Forward, QuotedString, Literal
+from pyparsing import Optional, Word, Suppress, nums, Group, delimitedList, Dict, Forward, QuotedString, Literal
 
 LBRACE, RBRACE, LBRACK, RBRACK, COLON, COMMA = map(Suppress, '{}[]:,')
 
@@ -11,10 +11,12 @@ real = Word('-.' + nums).setParseAction(lambda x: float(x[0]))
 
 string = QuotedString('"')
 value = Forward()
-array = Group(LBRACK + delimitedList(value) + RBRACK) # value is delimiter ([ value ])
-member = Group(string + COLON + value) # colon is delimiter (string : value)
-json_object = Dict(LBRACE + delimitedList(member) + RBRACE)
+array = Forward() # value is delimiter ([ value ])
+member = Forward() # colon is delimiter (string : value)
+json_object = Dict(LBRACE + Optional(delimitedList(member)) + RBRACE)
 
+array << Group(LBRACK + Optional(delimitedList(value)) + RBRACK)
+member << Group(string + COLON + value)
 value << (real | integer | string | json_object | array | TRUE | FALSE | NULL)
 
 
--- first-course/informatics/lab4/additional_task3/parse_xml.py
+++ first-course/informatics/lab4/additional_task3/parse_xml.py
@@ -1,9 +1,13 @@
-from dicttoxml import dicttoxml
+# import xmltodict
+import dicttoxml
 from xml.dom.minidom import parseString
 
 
-def parse_xml(data, result_file):
-    xml = dicttoxml(data, attr_type=False)
-    xml_string = parseString(xml).toprettyxml()
+def parse_xml(json_data, result_file):
+    print(json_data)
 
-    result_file.write(xml_string)
+    # json_data = {'root': json_data}
+    xml = dicttoxml.dicttoxml(json_data, attr_type=False)
+    parsedXml = parseString(xml)
+    # xmltodict.unparse(json_data, output=result_file, pretty=True)
+    result_file.write(parsedXml.toprettyxml())
