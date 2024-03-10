88f2d67a1a08e72f0fecbd093dd6bf187656969a
mip3x <plombiiiri@yandex.ru>
2024-03-10 11:45:08 -0700

    🍄 opd small changes

--- first-course/opd/lab5/bcomp.asm
+++ first-course/opd/lab5/bcomp.asm
@@ -3,15 +3,15 @@ eol: WORD 0x0A
 phrase: WORD 0x628
 
 START:
-		CLA
-		return: LD (phrase)+
-		CMP eol
-		BEQ exit
-		OUT 2
-		SWAB
-		OUT 2
-		JUMP return
-		exit: HLT
+    CLA
+    return: LD (phrase)+
+    CMP eol
+	BEQ exit
+	OUT 2
+	SWAB
+	OUT 2
+	JUMP return
+	exit: HLT
 
 ORG 0x628
 WORD 0x041B
