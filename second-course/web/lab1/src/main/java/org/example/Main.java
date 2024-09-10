eec24e7e02552b1a7fe33eba30d672eae5a52807
mip3x <plombiiiri@yandex.ru>
2024-09-10 11:58:31 +0000

    initial commit

---
+++ second-course/web/lab1/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+import com.fastcgi.FCGIInterface;
+
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    public static void main(String args[]) {
+        FCGIInterface fcgiInterface = new FCGIInterface();
+        while (fcgiInterface.FCGIaccept() >= 0) {
+
+            var content = """
+              <html>
+                <head><title>Java FastCGI Test App</title></head>
+                <body><h1>I <3 JAVA</h1></body>
+              </html>""";
+            var httpResponse = """
+              HTTP/1.1 228 OK
+              Content-Type: text/html
+              Content-Length: %d
+              
+              %s
+              """.formatted(content.getBytes(StandardCharsets.UTF_8).length, content);
+            System.out.println(httpResponse);
+        }
+    }
+}
