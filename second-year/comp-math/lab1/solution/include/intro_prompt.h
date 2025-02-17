1ada1c5a93b0877d81629ce1d3ac43cf4c52d2b7
mip3x <plombiiiri@yandex.ru>
2025-02-18 00:07:52 +0300

    feat: intro_prompt added

---
+++ second-year/comp-math/lab1/solution/include/intro_prompt.h
@@ -0,0 +1,9 @@
+#ifndef INTRO_PROMPT_H
+#define INTRO_PROMPT_H
+
+#define ST_NAME_ENV "ST_NAME"
+#define DEFAULT_ST_NAME "Иванов Иван Иванович"
+
+void print_intro_prompt();
+
+#endif
---
+++ second-year/comp-math/lab1/solution/src/main.c
@@ -0,0 +1,7 @@
+#include "../include/status.h"
+#include "../include/intro_prompt.h"
+
+int main() {
+    print_intro_prompt();
+    return SUCCESS;
+}
