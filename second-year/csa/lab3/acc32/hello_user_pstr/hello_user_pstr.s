beb566661ea3569ff76f48ca74453cb4967f5ca9
mip3x <plombiiiri@yandex.ru>
2025-04-12 16:35:39 +0300

    feat: hello_msg output

--- second-year/csa/lab3/acc32/hello_user_pstr/hello_user_pstr.s
+++ second-year/csa/lab3/acc32/hello_user_pstr/hello_user_pstr.s
@@ -1,47 +1,89 @@
     .data
 
-buffer:          .byte  '____________________'
+buf:             .byte  '________________________________'
+buf_size:        .word  0x20
+
 input_addr:      .word  0x80
 output_addr:     .word  0x84
 
 q_str:           .byte  'What is your name?\n'
-q_index:         .word  0
-q_ptr:           .word  0
 q_size:          .word  19
 
 hello_str:       .byte  'Hello, '
-excl_mark:       .byte  '!'
-buf_sise:        .word  0
+hello_size:      .word  7
 
+excl_mark:       .byte  '!'
+excl_mark_size:  .word  1
+
+index:           .word  0
+ptr:             .word  0
+buf_ptr:         .word  0
+
+const_0:         .word  0
 const_1:         .word  1
 const_FF:        .word  0xFF
-    
+buf_mask:        .word  0x5C5C5C00
 
     .text
 
 _start:
     load_imm     q_str
-    store        q_ptr
+    store        ptr
 
     load         q_size
-    store        q_index
+    store        index
 
 while_q_msg:
-    beqz         end
+    beqz         hello_msg_output
 
-    load_ind     q_ptr
+    load_ind     ptr
     and          const_FF
     store_ind    output_addr
 
-    load         q_ptr
+    load         ptr
     add          const_1
-    store        q_ptr
+    store        ptr
 
-    load         q_index
+    load         index
     sub          const_1
-    store        q_index
+    store        index
 
     jmp          while_q_msg
+
+hello_msg_output:
+    load_imm     buf
+    add          const_1        ; skip pascal length
+    store        buf_ptr
+
+    load_imm     hello_str
+    store        ptr
+
+    load         hello_size
+    store        index
+
+while_hello_msg:
+    beqz         read_line
+
+    load_ind     ptr
+    and          const_FF
+    or           buf_mask
+    store_ind    buf_ptr
+
+    load         ptr
+    add          const_1
+    store        ptr
+
+    load         buf_ptr
+    add          const_1
+    store        buf_ptr
+
+    load         index
+    sub          const_1
+    store        index
+
+    jmp          while_hello_msg
+
+read_line:
 
 end:
     halt
