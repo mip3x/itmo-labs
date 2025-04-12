a74950ccb691a8a036cbfe35a21fbc34a4c73225
mip3x <plombiiiri@yandex.ru>
2025-04-12 17:15:33 +0300

    feat: output msg from buffer

--- second-year/csa/lab3/acc32/hello_user_pstr/hello_user_pstr.s
+++ second-year/csa/lab3/acc32/hello_user_pstr/hello_user_pstr.s
@@ -12,7 +12,7 @@ q_size:          .word  19
 hello_str:       .byte  'Hello, '
 hello_size:      .word  7
 
-excl_mark:       .byte  '!'
+excl_mark:       .word  '!'
 excl_mark_size:  .word  1
 
 index:           .word  0
@@ -22,9 +22,12 @@ buf_ptr:         .word  0
 const_0:         .word  0
 const_1:         .word  1
 const_FF:        .word  0xFF
-buf_mask:        .word  0x5C5C5C00
+buf_mask:        .word  0x5F5F5F00
+const_newline:   .word  0x0A
+
 
     .text
+    .org 0x85
 
 _start:
     load_imm     q_str
@@ -84,6 +87,72 @@ while_hello_msg:
     jmp          while_hello_msg
 
 read_line:
+    load         buf_size
+    sub          const_1      ; pascal length in the beginning
+    sub          hello_size
+    sub          excl_mark_size
+    store        index
+
+while_read_line:
+    beqz         output_excl_mark
+
+    load_ind     input_addr
+
+    sub          const_newline          ; check if symbol == '\n'
+    beqz         output_excl_mark
+    add          const_newline
+
+    or           buf_mask
+    store_ind    buf_ptr
+
+    load         buf_ptr
+    add          const_1
+    store        buf_ptr
+
+    load         index
+    sub          const_1
+    store        index
+
+    jmp          while_read_line
+
+output_excl_mark:
+    load         excl_mark
+    and          const_FF
+    or           buf_mask
+    store_ind    buf_ptr
+    
+    load         buf_ptr
+    add          const_1
+    store        buf_ptr
+
+output_buf:
+    load_imm     buf
+    add          const_1
+    store        buf_ptr
+
+    load         const_0
+    store        index
+
+while_output_buf:
+    load         index
+    add          const_1
+    store        index
+
+    load_ind     buf_ptr
+    and          const_FF
+    store_ind    output_addr
+
+    sub          excl_mark
+    beqz         calc_pascal_str_length
+    add          excl_mark
+
+    load         buf_ptr
+    add          const_1
+    store        buf_ptr
+
+    jmp          while_output_buf
+
+calc_pascal_str_length:
 
 end:
     halt
