cedbc5d97736019e2d2d29701eb4edfd382d4199
mip3x <plombiiiri@yandex.ru>
2025-06-19 22:57:13 +0300

    refactor: MBeanRegistryUtil rewritten

--- second-year/opi/lab4/src/main/java/ru/mip3x/lab4/utils/MBeanRegistryUtil.java
+++ second-year/opi/lab4/src/main/java/ru/mip3x/lab4/utils/MBeanRegistryUtil.java
@@ -2,6 +2,7 @@ package ru.mip3x.lab4.utils;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import ru.mip3x.lab4.beans.PointStatsBean;
 import ru.mip3x.lab4.beans.ClickIntervalBean;
 
@@ -11,27 +12,29 @@ import java.lang.management.ManagementFactory;
 
 @ApplicationScoped
 public class MBeanRegistryUtil {
+    @Inject
+    private PointStatsBean pointStats;
+
+    @Inject
+    private ClickIntervalBean clickInterval;
 
     @PostConstruct
     public void init() {
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
         try {
-            MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-
-            ObjectName name1 = new ObjectName("ru.mip3x.lab4.beans:type=PointStats");
-            if (!mbs.isRegistered(name1)) {
-                mbs.registerMBean(new PointStatsBean(), name1);
-                System.out.println("registered PointStatsMBean");
-            }
-
-            ObjectName name2 = new ObjectName("ru.mip3x.lab4.beans:type=ClickInterval");
-            if (!mbs.isRegistered(name2)) {
-                System.out.println("registered ClickIntervalMBean");
-                mbs.registerMBean(new ClickIntervalBean(), name2);
-            }
-
+            register(pointStats, "lab4.beans:type=PointStats");
+            register(clickInterval, "lab4.beans:type=ClickInterval");
         } catch (Exception e) {
-            System.out.println("Cant register MBeans");
-            throw new RuntimeException("Не удалось зарегистрировать MBean’ы", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void register(Object bean, String name) throws Exception {
+        ObjectName on = new ObjectName(name);
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        if (!mbs.isRegistered(on)) {
+            mbs.registerMBean(bean, on);
+            System.out.println("registered " + name);
         }
     }
 }
