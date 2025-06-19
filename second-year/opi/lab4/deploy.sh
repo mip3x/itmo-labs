ab6d493dd885185b241b033ca6f006b456911b7f
mip3x <plombiiiri@yandex.ru>
2025-06-19 20:39:15 +0300

    chore: artifacts from lab3 deleted

--- second-year/opi/lab4/deploy.sh
+++ second-year/opi/lab4/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-# ./gradlew clean build
-# cp ./build/libs/lab4.war ../wildfly/standalone/deployments/ROOT.war
-cp ./build/artifacts/lab4*.war ../wildfly/standalone/deployments/ROOT.war
+./gradlew clean build
+cp ./build/libs/lab4.war ../../web/wildfly/standalone/deployments/ROOT.war
--- second-year/opi/lab4/src/main/java/ru/mip3x/lab4/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.mip3x.lab4;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("!!! NEW MAIN CLASS !!!");
-    }
-}
