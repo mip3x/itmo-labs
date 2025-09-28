9ee5594983ceabc8ad424406713f6cdecf14c1c5
mip3x <plombiiiri@yandex.ru>
2025-09-29 02:53:28 +0300

    chore: disabling SpringBootTest

--- third-year/information-systems/lab1/src/test/java/ru/mip3x/isLab1/IsLab1ApplicationTests.java
+++ third-year/information-systems/lab1/src/test/java/ru/mip3x/isLab1/IsLab1ApplicationTests.java
@@ -1,12 +1,13 @@
 package ru.mip3x.isLab1;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class IsLab1ApplicationTests {
 
-	@org.junit.jupiter.api.Disabled("Disabled in CI")
 	@Test
 	void contextLoads() {
 	}
