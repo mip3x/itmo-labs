fe6635aca0f8ac3d3547bff3af16cc16abf67138
mip3x <plombiiiri@yandex.ru>
2025-09-28 17:36:40 +0300

    feat: initial lab1 is

---
+++ third-year/information-systems/lab1/src/main/java/ru/mip3x/isLab1/IsLab1Application.java
@@ -0,0 +1,13 @@
+package ru.mip3x.isLab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsLab1Application.class, args);
+	}
+
+}
---
+++ third-year/information-systems/lab1/src/test/java/ru/mip3x/isLab1/IsLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mip3x.isLab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
