2af34a42977c36592434c51517341e602d48bd7f
mip3x <plombiiiri@yandex.ru>
2025-09-29 12:06:11 +0300

    fix: CORS enabled in controller

--- third-year/information-systems/lab1/backend/src/main/java/ru/mip3x/controller/PersonController.java
+++ third-year/information-systems/lab1/backend/src/main/java/ru/mip3x/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,7 @@ import ru.mip3x.mapper.PersonMapper;
 import ru.mip3x.model.Person;
 import ru.mip3x.service.PersonService;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/persons")
 @AllArgsConstructor
