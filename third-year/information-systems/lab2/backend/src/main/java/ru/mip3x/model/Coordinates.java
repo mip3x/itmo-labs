57cbadf846a3b0d0cb9bb8870549f2bc98141188
mip3x <plombiiiri@yandex.ru>
2025-12-22 03:41:48 +0300

    chore: move lab2 work into lab2 directory; restore lab1 from main

---
+++ third-year/information-systems/lab2/backend/src/main/java/ru/mip3x/model/Coordinates.java
@@ -0,0 +1,31 @@
+package ru.mip3x.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "coordinates")
+public class Coordinates {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Min(value = -859)
+    private double x;
+
+    @NotNull
+    @Column(nullable = false)
+    @Max(value = 396)
+    private Float y;
+}
