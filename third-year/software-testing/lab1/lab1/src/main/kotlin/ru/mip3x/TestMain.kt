c30c8a339aa76503b3eb5b8a56d167249e5a1d99
mip3x <plombiiiri@yandex.ru>
2026-02-24 03:30:11 +0300

    feat: b-tree done

--- third-year/software-testing/lab1/lab1/src/main/kotlin/ru/mip3x/TestMain.kt
+++ third-year/software-testing/lab1/lab1/src/main/kotlin/ru/mip3x/TestMain.kt
@@ -1,15 +1,27 @@
 package ru.mip3x
 
+import ru.mip3x.algo.BTree
 import ru.mip3x.series.SinSeries
 import kotlin.math.sin
 
 fun main() {
-    val x = 0.5
-    val eps = 1e-10
-    val approx = SinSeries.sinTaylor(x, eps).value
-    val real = sin(x)
+    // val x = 0.5
+    // val eps = 1e-10
+    // val approx = SinSeries.sinTaylor(x, eps).value
+    // val real = sin(x)
+    //
+    // println("approx = $approx")
+    // println("real   = $real")
+    // println("diff   = ${kotlin.math.abs(approx - real)}")
 
-    println("approx = $approx")
-    println("real   = $real")
-    println("diff   = ${kotlin.math.abs(approx - real)}")
+    val tree = BTree()
+
+    val values = listOf(10, 80, 100, 25, 20, 55, 35, 70, 30, 40, 13, 12, 50, 26, 60, 22, 61)
+    for (v in values) {
+        tree.insert(v)
+        println("insert $v -> ${tree.inOrder()}")
+    }
+
+    println("contains(30) = ${tree.contains(30)}")
+    println("contains(999) = ${tree.contains(999)}")
 }
--- third-year/software-testing/lab1/lab1/src/main/kotlin/ru/mip3x/algo/BTree.kt
+++ third-year/software-testing/lab1/lab1/src/main/kotlin/ru/mip3x/algo/BTree.kt
@@ -1,1 +1,126 @@
-package ru.mip3x.algo.btree
+package ru.mip3x.algo
+
+class BTree {
+    private class Node(
+        var isLeaf: Boolean
+    ) {
+        val keys = mutableListOf<Int>()
+        val children = mutableListOf<Node>()
+    }
+
+    private var root: Node = Node(isLeaf = true)
+    private val maxKeys: Int = 5
+
+    fun contains(key: Int) : Boolean {
+        return contains(root, key)
+    }
+
+    private fun contains(node: Node, key: Int) : Boolean {
+        for (i in 0 until node.keys.size) {
+            if (key <= node.keys[i]) {
+                if (key == node.keys[i])
+                    return true
+                if (node.isLeaf)
+                    return false
+                return contains(node.children[i], key)
+            }
+        }
+
+        if (node.isLeaf)
+            return false
+
+        return contains(node.children.last(), key)
+    }
+
+    fun insert(key: Int) : Boolean {
+        if (contains(root, key))
+            return false
+        
+        if (root.keys.size == maxKeys) {
+            var newRoot = Node(isLeaf = false)
+            newRoot.children.add(root)
+            splitChild(newRoot, 0)
+            root = newRoot
+        }
+
+        insertNonFull(root, key)
+        return true;
+    }
+
+    private fun splitChild(parent: Node, childIndex: Int) {
+        val fullChild = parent.children[childIndex]
+        val rightChild = Node(isLeaf = fullChild.isLeaf)
+
+        val mid = maxKeys / 2
+        val median = fullChild.keys[mid] // key that will be up in parent
+
+        // right keys
+        for (i in mid + 1 until fullChild.keys.size)
+            rightChild.keys.add(fullChild.keys[i])
+
+        // if not leaf, right children are transferred also
+        if (!fullChild.isLeaf) {
+            for (i in mid + 1 until fullChild.children.size)
+                rightChild.children.add(fullChild.children[i])
+        }
+
+        // delete right keys from left (old) node
+        fullChild.keys.subList(mid, fullChild.keys.size).clear()
+
+        if (!fullChild.isLeaf) {
+            fullChild.children.subList(mid + 1, fullChild.children.size).clear()
+        }
+
+        // lift median to parent
+        parent.keys.add(childIndex, median)
+
+        parent.children.add(childIndex + 1, rightChild)
+    }
+
+    private fun insertNonFull(node: Node, key: Int) {
+        var i = node.keys.size - 1
+
+        if (node.isLeaf) {
+            node.keys.add(key)
+            while (i >= 0 && key < node.keys[i]) {
+                node.keys[i + 1] = node.keys[i]
+                i--
+            }
+            node.keys[i + 1] = key
+            return
+        }
+
+        while (i >= 0 && key < node.keys[i]) {
+            i--
+        }
+        i++
+
+        if (node.children[i].keys.size == maxKeys) {
+            splitChild(node, i)
+            if (key > node.keys[i]) {
+                i++
+            }
+        }
+
+        insertNonFull(node.children[i], key)
+    }
+
+    fun inOrder(): List<Int> {
+        val result = mutableListOf<Int>()
+        inOrder(root, result)
+        return result
+    }
+
+    private fun inOrder(node: Node, out: MutableList<Int>) {
+        // add children fewer than last key
+        for (i in 0 until node.keys.size) {
+            if (!node.isLeaf)
+                inOrder(node.children[i], out)
+            out.add(node.keys[i])
+        }
+
+        // add last children (the biggest)
+        if (!node.isLeaf)
+            inOrder(node.children.last(), out)
+    }
+}
