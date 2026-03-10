a9ec6ae1b5f2b00a409b931b5ef047010b0f7865
mip3x <plombiiiri@yandex.ru>
2026-03-10 21:13:12 +0300

    feat: parameterized test added

--- third-year/software-testing/lab1/lab1/src/test/kotlin/ru/mip3x/series/SinSeriesTest.kt
+++ third-year/software-testing/lab1/lab1/src/test/kotlin/ru/mip3x/series/SinSeriesTest.kt
@@ -2,164 +2,127 @@ package ru.mip3x.series
 
 import ru.mip3x.series.SinSeries
 
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator
+import org.junit.jupiter.params.aggregator.AggregateWith
+import org.junit.jupiter.api.extension.ParameterContext
 
-import kotlin.math.PI
-import kotlin.math.sqrt
-import kotlin.math.sin
-import kotlin.math.abs
+import java.util.stream.Stream
+
+data class SinCase(
+    val x: Double,
+    val expected: Double
+)
+
+class SinCaseAggregator : ArgumentsAggregator {
+    override fun aggregateArguments(accessor: ArgumentsAccessor, context: ParameterContext): Any {
+        return SinCase(
+            accessor.getDouble(0),
+            accessor.getDouble(1)
+        )
+    }
+}
 
 class SinSeriesTest {
     // eps equivalent classes
-    @Test
-    fun errorOnZeroEps() {
+    @ParameterizedTest(name = "throw err for eps in {0}")
+    @ValueSource(doubles = [0.0, -1.0])
+    fun errorOnInvalidEps(eps: Double) {
         assertThrows<IllegalArgumentException> {
-            SinSeries.sinTaylor(0.5, 0.0)
-        }
-    }
-
-    @Test
-    fun errorOnNegativeEps() {
-        assertThrows<IllegalArgumentException> {
-            SinSeries.sinTaylor(0.5, -1.0)
+            SinSeries.sinTaylor(0.5, eps)
         }
     }
 
     // X equivalent classes
-    @Test
-    fun errorOnZeroX() {
-        val r = SinSeries.sinTaylor(0.0, 1e-12).value
-        assertEquals(0.0, r, 1e-15)
+    @ParameterizedTest(name = "sinTaylor({0}) = {1}")
+    @MethodSource("sinTaylorCases")
+    fun sinTaylorReturnsExpectedValue(
+        @AggregateWith(SinCaseAggregator::class) case: SinCase
+    ) {
+        val actual = SinSeries.sinTaylor(case.x, 1e-12).value
+        assertEquals(case.expected, actual, 1e-12)
     }
 
-    @Test
-    fun sinPiOver4() {
-        val r: Double = SinSeries.sinTaylor(PI / 4, 1e-12).value
-        val expected: Double = sqrt(2.0) / 2
-        assertEquals(expected, r, 1e-12)
+    companion object {
+        @JvmStatic
+        fun sinTaylorCases(): Stream<Arguments> = Stream.of(
+            Arguments.of(0.0, 0.0),
+            Arguments.of(3.141592653589793 / 4, 1.4142135623730951 / 2),
+            Arguments.of(3.141592653589793 / 2, 1.0),
+            Arguments.of((3 * 3.141592653589793) / 4, 1.4142135623730951 / 2),
+            Arguments.of(3.141592653589793, 0.0),
+            Arguments.of((5 * 3.141592653589793) / 4, -(1.4142135623730951 / 2)),
+            Arguments.of((3 * 3.141592653589793) / 2, -1.0),
+            Arguments.of((7 * 3.141592653589793) / 4, -(1.4142135623730951 / 2)),
+            Arguments.of(2 * 3.141592653589793, 0.0),
+            Arguments.of((15 * 3.141592653589793) / 4, -(1.4142135623730951 / 2)),
+            Arguments.of(-3 * 3.141592653589793 / 2, 1.0)
+        )
     }
 
-    @Test
-    fun sinPiOver2() {
-        val r: Double = SinSeries.sinTaylor(PI / 2, 1e-12).value
-        val expected: Double = 1.0
-        assertEquals(expected, r, 1e-12)
-    }
-
-    @Test
-    fun sin3PiOver4() {
-        val r: Double = SinSeries.sinTaylor((3 * PI) / 4, 1e-12).value
-        val expected: Double = sqrt(2.0) / 2
-        assertEquals(expected, r, 1e-12)
-    }
-
-    @Test
-    fun sinPi() {
-        val r: Double = SinSeries.sinTaylor(PI, 1e-12).value
-        val expected: Double = 0.0
-        assertEquals(expected, r, 1e-12)
-    }
-
-    @Test
-    fun sin5PiOver4() {
-        val r: Double = SinSeries.sinTaylor((5 * PI) / 4, 1e-12).value
-        val expected: Double = -(sqrt(2.0) / 2)
-        assertEquals(expected, r, 1e-12)
-    }
-    
-    @Test
-    fun sin3PiOver2() {
-        val r: Double = SinSeries.sinTaylor((3 * PI) / 2, 1e-12).value
-        val expected: Double = -1.0
-        assertEquals(expected, r, 1e-12)
-    }
-
-    @Test
-    fun sin7PiOver4() {
-        val r: Double = SinSeries.sinTaylor((7 * PI) / 4, 1e-12).value
-        val expected: Double = -(sqrt(2.0) / 2)
-        assertEquals(expected, r, 1e-12)
-    }
-
-    @Test
-    fun sin2PI() {
-        val r: Double = SinSeries.sinTaylor(2 * PI, 1e-12).value
-        val expected: Double = 0.0
-        assertEquals(expected, r, 1e-12)
-    }
-
-    @Test
-    fun sin15PiOver4() {
-        val r: Double = SinSeries.sinTaylor((15 * PI) / 4, 1e-12).value
-        val expected: Double = -(sqrt(2.0) / 2)
-        assertEquals(expected, r, 1e-12)
-    }
-
+    // termsGetter
     @Test
     fun termsGetterIsCovered() {
-        val res = SinSeries.sinTaylor(PI / 4, 1e-12)
+        val res = SinSeries.sinTaylor(3.141592653589793 / 4, 1e-12)
         assertTrue(res.terms > 0)
     }
 
-    @Test
-    fun reducesAngleWhenLessThanMinusPi() {
-        val r: Double = SinSeries.sinTaylor(-3 * PI / 2, 1e-12).value
-        val expected: Double = 1.0
-        assertEquals(expected, r, 1e-12)
-    }
-
     // factorial
-    @Test
-    fun factorialThrowsOnNegative() {
+    @ParameterizedTest(name = "factorial should throw for n = {0}")
+    @ValueSource(ints = [-1])
+    fun factorialThrowsOnNegative(n: Int) {
         assertThrows<IllegalArgumentException> {
-            SinSeries.factorial(-1)
+            SinSeries.factorial(n)
         }
     }
 
-    @Test
-    fun factorialNoLoopFor0And1() {
-        assertEquals(1.0, SinSeries.factorial(0), 0.0)
-        assertEquals(1.0, SinSeries.factorial(1), 0.0)
-    }
-
-    @Test
-    fun factorialLoopExecutesFor2Plus() {
-        assertEquals(2.0, SinSeries.factorial(2), 0.0)
-        assertEquals(6.0, SinSeries.factorial(3), 0.0)
+    @ParameterizedTest(name = "factorial({0}) = {1}")
+    @CsvSource(
+        "0, 1.0",
+        "1, 1.0",
+        "2, 2.0",
+        "3, 6.0"
+    )
+    fun factorialReturnsExpectedValue(args: ArgumentsAccessor) {
+        assertEquals(args.getDouble(1), SinSeries.factorial(args.getInteger(0)), 0.0)
     }
 
     // pow
-    @Test
-    fun powThrowsOnNegative() {
+    @ParameterizedTest(name = "pow should throw for n = {0}")
+    @ValueSource(ints = [-1])
+    fun powThrowsOnNegative(n: Int) {
         assertThrows<IllegalArgumentException> {
-            SinSeries.pow(2.0, -1)
+            SinSeries.pow(2.0, n)
         }
     }
 
-    @Test
-    fun powRepeatZeroTimesWhenNIs0() {
-        assertEquals(1.0, SinSeries.pow(123.0, 0), 0.0)
-    }
-
-    @Test
-    fun powRepeatExecutesWhenNPositive() {
-        assertEquals(8.0, SinSeries.pow(2.0, 3), 1e-15)
-        assertEquals(0.25, SinSeries.pow(0.5, 2), 1e-15)
+    @ParameterizedTest(name = "pow({0}, {1}) = {2}")
+    @CsvSource(
+        "123.0, 0, 1.0",
+        "2.0, 3, 8.0",
+        "0.5, 2, 0.25"
+    )
+    fun powReturnsExpectedValue(x: Double, n: Int, expected: Double) {
+        assertEquals(expected, SinSeries.pow(x, n), 1e-15)
     }
 
     // abs
-    @Test
-    fun absPositiveBranch() {
-        assertEquals(2.5, SinSeries.abs(2.5), 0.0)
-    }
-
-    @Test
-    fun absNonPositiveBranch() {
-        assertEquals(2.5, SinSeries.abs(-2.5), 0.0)
-        assertEquals(-0.0, SinSeries.abs(0.0), 0.0)
+    @ParameterizedTest(name = "abs({0}) = {1}")
+    @CsvSource(
+        "2.5, 2.5",
+        "-2.5, 2.5",
+        "0.0, 0.0"
+    )
+    fun absReturnsExpectedValue(x: Double, expected: Double) {
+        assertEquals(expected, SinSeries.abs(x), 0.0)
     }
 }
